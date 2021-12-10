1d359fb29b57f85cadc12dfa3059c5d9dd1b04c4
hyeri01 <fia_re@naver.com>
2021-12-10 10:54:57 +0900

    init

---
+++ src/main/java/com/ex_community/ExCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ex_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ex_community/ExCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
