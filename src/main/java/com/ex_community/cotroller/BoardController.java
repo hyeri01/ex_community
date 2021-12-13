9a061d114edb6d410ca42e7f9f1dcf8ea5b23450
hyeri01 <fia_re@naver.com>
2021-12-13 12:34:37 +0900

    BoardValidator 추가. 등록 혹은 수정 시, 조건에 맞지 않으면 Error message 출력

--- src/main/java/com/ex_community/cotroller/BoardController.java
+++ src/main/java/com/ex_community/cotroller/BoardController.java
@@ -2,14 +2,17 @@ package com.ex_community.cotroller;
 
 import com.ex_community.model.Board;
 import com.ex_community.repository.BoardRepository;
+import com.ex_community.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -19,6 +22,9 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model){
         List<Board> boards = boardRepository.findAll();
@@ -27,13 +33,25 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model){
-        model.addAttribute("board", new Board());
+    public String form(Model model, @RequestParam(required = false) Long id){
+        // required 설정 이유 => 필요한 경우에만 받기 위해
+
+        if (id == null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            // key 값을 기준으로 조회 || orElse => 값이 없을 경우 null로
+            model.addAttribute("board", board); // 값이 있는 경우
+        }
         return "board/form";
     }
 
     @PostMapping("/form")
-    public String fromSubmit(@ModelAttribute Board board){
+    public String fromSubmit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
         boardRepository.save(board); // save == insert
         // save == id의 key 값이 있을 경우에는 update가, 없는 경우에는 insert
 
--- src/main/java/com/ex_community/model/Board.java
+++ src/main/java/com/ex_community/model/Board.java
@@ -1,10 +1,13 @@
 package com.ex_community.model;
 
 import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -14,6 +17,8 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 위해 설정
     private Long id;
 
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하로 입력하실 수 있습니다.")
     private String title;
     private String content;
 }
