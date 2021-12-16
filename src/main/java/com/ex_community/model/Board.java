e8334e8a0aa20191512c1a52edc8e07c045547ba
hyeri01 <fia_re@naver.com>
2021-12-16 12:08:59 +0900

    board table과 user table join을 통해 글 작성자 가져오기

--- src/main/java/com/ex_community/model/Board.java
+++ src/main/java/com/ex_community/model/Board.java
@@ -2,10 +2,7 @@ package com.ex_community.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,4 +18,14 @@ public class Board {
     @Size(min=2, max=30, message = "제목은 2자 이상 30자 이하로 입력하실 수 있습니다.")
     private String title;
     private String content;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    /* name = 어떤 컬럼과 user table이 연결이 될 지 설정 */
+    /* referencedColumnName = 사용자 table 어떤 컬럼과 연결되어 있는지 */
+    /* referencedColumnName의 경우 @ID인 pk 값으로 지정되어 있으면 생략해도 괜찮다*/
+    private User user;
+
+
 }
